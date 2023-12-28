3d25a31ef3484af88684e78f5a4cf5be82bcf506
ayshasiddiqua513 <22it026@skcet.ac.in>
2023-12-28 21:17:12 +0530

    Manipulating the DOM - React States

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,22 +1,23 @@
+import React,{ useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetail from './ExpenseDetail';
 import Card from './UI/Card';
 import './ExpenseItem.css';
 function ExpenseItem(props){
-//    const expenseDate=new Date(2023/12/24);
-//     const expenseTitle='Bike petrol';
-//     const expenseAmount=200;
-//     const LocationOfExpenditure='Koundampalayam';
+
+    const [title,setTitle]=useState(props.title);
+
      
     const clickHander=()=>{
-        console.log('clicked!!!');
+        setTitle('Updated');
+        console.log(setTitle);
     }; 
 
     return( 
     <Card className='expense-item'>
          <ExpenseDate date={props.date}/> 
-        <ExpenseDetail amount= {props.amount}  location={props.location}   title={props.title}  />
-        <button onClick={clickHander}>Display</button>
+        <ExpenseDetail amount= {props.amount}  location={props.location}   title={title}  />
+        <button onClick={clickHander}>Change title</button>
     </Card>
     );
 }
